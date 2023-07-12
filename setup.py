3813d959f1f58fa152aab6b7a0fb1ef4f608fcff
pquigle <113115134+pquigle@users.noreply.github.com>
2023-07-12 10:16:53 -0500

    Added auto-detection of packages and version number to setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,28 @@ from setuptools import setup, find_packages, Extension
 import numpy, sys
 import re
 
+# auto-updating version code stolen from RadVel
+def get_property(prop, project):
+    result = re.search(
+        r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+        open(project + "/__init__.py").read(),
+    )
+    return result.group(1)
+
+def get_requires():
+    reqs = []
+    for line in open("requirements.txt", "r").readlines():
+        reqs.append(line)
+    return reqs
+
 setup(
     name="DEBRaVE",
-    version="0.0.1",
+    version=get_property("__version__", "orbitize"),
     description="DEBRaVE: Detached Eclipse Binary Radial Velocity Extractor",
     url="https://github.com/ganesh1076/DEBRaVE",
     author="",
     author_email="",
     packages=find_packages(),
-    keywords="Astronomy Binaries Radial Velocity Spectroscopy TODCOR"
+    keywords="Astronomy Binaries Radial Velocity Spectroscopy TODCOR",
+    install_requires=get_requires(),
 )
